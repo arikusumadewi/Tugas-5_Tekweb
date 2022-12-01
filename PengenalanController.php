ab5735d982bb455a86ed4a516dfc0c71da64f1a0
arikusumadewi <114129833+arikusumadewi@users.noreply.github.com>
2022-12-01 17:27:23 +0700

    Create PengenalanController.php

---
+++ PengenalanController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PenganalanController extends Controller
+{
+    //
+    public function index(){
+        return view('kecak');
+    }
+}
